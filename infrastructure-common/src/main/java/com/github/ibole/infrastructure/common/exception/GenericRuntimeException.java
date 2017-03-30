eaa33bab706765e8b764741bfd4ada4207c4c9ef
benson-git <chikaiwang@hotmail.com>
2017-03-30 16:22:44 +0800

    Update - recompile proto files in common package

--- infrastructure-common/src/main/java/com/github/ibole/infrastructure/common/exception/GenericRuntimeException.java
+++ infrastructure-common/src/main/java/com/github/ibole/infrastructure/common/exception/GenericRuntimeException.java
@@ -3,12 +3,15 @@
  */
 package com.github.ibole.infrastructure.common.exception;
 
-/*********************************************************************************************.
+/*********************************************************************************************
+ * .
  * 
  * 
- * <p>Copyright 2016, iBole Inc. All rights reserved.
+ * <p>
+ * Copyright 2016, iBole Inc. All rights reserved.
  * 
- * <p></p>
+ * <p>
+ * </p>
  *********************************************************************************************/
 
 
@@ -19,11 +22,11 @@ package com.github.ibole.infrastructure.common.exception;
 public class GenericRuntimeException extends RuntimeException {
 
   private static final long serialVersionUID = 232350690610319469L;
-  
+
   public GenericRuntimeException(String msg, Throwable ex) {
     super(msg, ex);
   }
-  
+
   public GenericRuntimeException(Throwable ex) {
     super(ex);
   }
